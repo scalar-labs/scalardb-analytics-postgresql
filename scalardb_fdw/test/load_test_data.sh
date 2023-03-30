325fcb09ea9325877c86bc0da22bede6948714bd
Akihiro Okuno <akihiro.okuno@scalar-labs.com>
2023-03-30 12:29:06 +0900

    Skip loading test data if it has been loaded before

--- scalardb_fdw/test/load_test_data.sh
+++ scalardb_fdw/test/load_test_data.sh
@@ -4,13 +4,32 @@ set -eu
 
 script_dir=$(dirname "$0")
 
-echo '- load ScalarDB schemas'
-java -jar "${script_dir}/scalardb-schema-loader-3.8.0.jar" \
-    --config "${script_dir}/client.properties" \
-    -f "${script_dir}/schema.json" \
-    --coordinator
+namespace_to_check="cassandrans"
+table_to_check="test"
 
-echo '- load test data into ScalarDB backends'
-java -jar "${script_dir}/scalardb-sql-cli-3.8.0-all.jar" \
-    --config "${script_dir}/client.properties" \
-    -f "${script_dir}/test-data.sql"
+check_if_schema_is_loaded() {
+    echo '- Check if test data is loaded'
+    java -jar "${script_dir}/scalardb-sql-cli-3.8.0-all.jar" \
+        --config "${script_dir}/client.properties" \
+        --output-format=json\
+        --execute="show tables from $namespace_to_check" 2>/dev/null | \
+    jq -r .resultset[].tableName | \
+    grep "$table_to_check" > /dev/null
+
+    return $?
+}
+
+if check_if_schema_is_loaded; then
+    echo "- Test data has already been loaded. Skip loading test data"
+else
+    echo '- Load ScalarDB schemas'
+    java -jar "${script_dir}/scalardb-schema-loader-3.8.0.jar" \
+        --config "${script_dir}/client.properties" \
+        -f "${script_dir}/schema.json" \
+        --coordinator
+
+    echo '- Load test data into ScalarDB backends'
+    java -jar "${script_dir}/scalardb-sql-cli-3.8.0-all.jar" \
+        --config "${script_dir}/client.properties" \
+        -f "${script_dir}/test-data.sql"
+fi
