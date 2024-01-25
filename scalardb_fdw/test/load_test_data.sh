93d9215d47eedc13a0edd003a01d643592a01252
Akihiro Okuno <akihiro.okuno@scalar-labs.com>
2024-01-25 13:27:49 +0900

    Revise script setting up testing environment (#51)

--- scalardb_fdw/test/load_test_data.sh
+++ scalardb_fdw/test/load_test_data.sh
@@ -4,4 +4,4 @@ set -eu
 
 script_dir=$(dirname "$0")
 properties_path=$(readlink -f "$script_dir/client.properties")
-./gradlew test-data-loader:run --args $properties_path
+"$script_dir/../gradlew" -p "$script_dir/.." test-data-loader:run --args "$properties_path"
